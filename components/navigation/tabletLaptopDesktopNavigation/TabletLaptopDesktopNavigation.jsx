022ebdabaf96df21d3f1b10cd60c5ef41f9f1b59
Tanya Woodside <tanya.m.woodside@gmail.com>
2023-12-11 10:06:06 -0800

    Add styles for hovered and active menu items

--- components/navigation/tabletLaptopDesktopNavigation/TabletLaptopDesktopNavigation.jsx
+++ components/navigation/tabletLaptopDesktopNavigation/TabletLaptopDesktopNavigation.jsx
@@ -1,5 +1,6 @@
 'use client';
 import styles from './TabletLaptopDesktopNavigation.module.scss';
+import { useState } from 'react';
 import Link from 'next/link';
 import SiteName from '../SiteName';
 import { useCurrentNavigationPath } from '@/hooks/useCurrentNavigationPath';
@@ -7,17 +8,29 @@ import { navigationLinks } from '../navigationLinks';
 
 export default function TabletLaptopDesktopNavigation() {
   const currentPathname = useCurrentNavigationPath();
+  const [hoveredNavLink, setHoveredNavLink] = useState(null);
+
+  const handleHoverNavLink = (link) => setHoveredNavLink(link);
+  const handleBlurNavLink = () => setHoveredNavLink(null);
+  console.log('HOVERED LINK: ', hoveredNavLink);
 
   return (
     <div className={styles.navBar}>
       <SiteName />
       <ul className={styles.navMenu}>
         {navigationLinks.map((link) => (
-          <li key={link} className={styles.navMenuItem}>
+          <li
+            key={link}
+            className={styles.navMenuItem}
+            onPointerEnter={() => handleHoverNavLink(link)}
+            onPointerLeave={handleBlurNavLink}>
             <Link
               href={link === 'home' ? '/' : `/${link}`}
-              className={styles.navMenuLink}
-              style={{ color: link === currentPathname ? 'black' : 'purple' }}>
+              className={
+                link === currentPathname
+                  ? `${styles.navMenuLink} ${styles.active}`
+                  : styles.navMenuLink
+              }>
               {link}
             </Link>
           </li>
